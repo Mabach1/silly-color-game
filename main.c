1f233143e37446fcaa9beada9a1fe622d4d4cade
Mabach1 <matej.baliga@gmail.com>
2023-06-27 22:44:06 +0200

    adding definition of Color enum and getting the char value

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
 #include "base/base.h"
 
+typedef enum {
+    Red,
+    Green, 
+    Blue,
+    Cyan,
+    Magenta,
+    Yellow,
+    White,
+    Black,
+    Undefined
+} Color;
+
+const char *get_color_label(Color color) {
+    switch (color) {
+        case Red: return "red";
+        case Green: return "green";
+        case Blue: return "blue";
+        case Cyan: return "cyan";
+        case Magenta: return "magenta";
+        case Yellow: return "yellow";
+        case White: return "white";
+        case Black: return "black";
+
+        default: return "undefined";
+    }
+
+    return "undefined";
+}
+
 i32 main(void) {
     Arena arena;    
 
