8b84c6c4f25d8033a605c226545e928da5d692f2
Mabach1 <matej.baliga@gmail.com>
2023-06-27 23:01:51 +0200

    generating random sequence of colors and displaying them

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "base/base.h"
 
+#define NUM_GUESS   (4)
+
 typedef enum {
     Red,
     Green, 
@@ -12,7 +14,7 @@ typedef enum {
     Undefined
 } Color;
 
-const char *get_color_label(Color color) {
+const char *color_label(Color color) {
     switch (color) {
         case Red: return "red";
         case Green: return "green";
@@ -29,10 +31,44 @@ const char *get_color_label(Color color) {
     return "undefined";
 }
 
+u64 num_colors(void) {
+    u64 result = 0;
+
+    for (Color color = 0; color < Undefined; ++color) {
+        ++result;
+    }
+
+    return result;
+}
+
+Color *secret_init(Arena *arena) {
+    u64 random_numbers[NUM_GUESS];
+
+    ran_rand_range_arr(random_numbers, NUM_GUESS, 0, num_colors() - 1, true);
+
+    Color *result = arena_alloc(arena, NUM_GUESS);
+
+    for (usize i = 0; i < NUM_GUESS; ++i) {
+        result[i] = random_numbers[i];
+    }
+
+    return result;
+}
+
+void hint(const Color *secret) {
+    for (usize i = 0; i < NUM_GUESS; ++i) {
+        fprintf(stdout, "%s ", color_label(secret[i]));
+    }
+}
+
 i32 main(void) {
     Arena arena;    
 
     arena_init(&arena);
+
+    Color *secret = secret_init(&arena);
+
+    hint(secret);
 
     arena_deinit(&arena);
 
