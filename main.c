b973af36a54b731c5e3d2c8e24d99871c4ad5062
Mabach1 <matej.baliga@gmail.com>
2023-06-27 23:21:38 +0200

    getting the colors from the standard input

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ typedef enum {
     Undefined
 } Color;
 
-const char *color_label(Color color) {
+const char *get_color_label(Color color) {
     switch (color) {
         case Red: return "red";
         case Green: return "green";
@@ -53,8 +53,46 @@ void secret_init(Color secret_buffer[NUM_GUESS]) {
 
 void hint(const Color *secret) {
     for (usize i = 0; i < NUM_GUESS; ++i) {
-        fprintf(stdout, "%s ", color_label(secret[i]));
+        fprintf(stdout, "%s ", get_color_label(secret[i]));
     }
+}
+
+usize get_color_index(String label) {
+    for (Color color = 0; color < Undefined; ++color) {
+        if (string_equal(label, string_str_lit(get_color_label(color)))) {
+            return color;
+        }
+    }
+    return Undefined;
+}
+
+bool player_guess(Color guess_buffer[NUM_GUESS]) {
+    Arena scratch; 
+
+    arena_init(&scratch);
+
+    StringArr inputs = stringarr_alloc(&scratch, NUM_GUESS);
+
+    for (usize i = 0; i < NUM_GUESS; ++i) {
+        fprintf(stdout, "%llu. color: ", i + 1);
+
+        String input = string_from_stdin(&scratch);
+
+        // TODO:
+        // if (!check_input()) {
+
+        // }
+
+        stringarr_push(&inputs, input);
+    }
+
+    for (usize i = 0; i < NUM_GUESS; ++i) {
+        guess_buffer[i] = get_color_index(inputs.arr[i]);
+    }
+
+    arena_deinit(&scratch);
+
+    return true;
 }
 
 i32 main(void) {
@@ -67,6 +105,14 @@ i32 main(void) {
     secret_init(secret);
 
     hint(secret);
+
+    Color guess[NUM_GUESS];
+
+    player_guess(guess);
+
+    for (usize i = 0; i < NUM_GUESS; ++i) {
+        fprintf(stdout, "%s ", get_color_label(guess[i]));
+    }
 
     arena_deinit(&arena);
 
